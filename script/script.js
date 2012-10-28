84ae04d2ca30bf48d595858d1c9328b7129f2960
Alex Wilson <arexkun11@gmail.com>
2012-10-28 13:18:07 -0400

    add some fun, random effects to the author name and fix the URL for search.min.js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,12 @@
 ;(function () {
-  var search = document.getElementById('search')
-  var results = document.getElementById('results')
+
+  function $ (id) {return document.getElementById(id)}
+
+  var search = $('search')
+  var results = $('results')
   var search_input = search.firstChild
   var loading = search.childNodes[1]
-  var search_worker = wrench.init('script/search.min.js')
+  var search_worker = wrench.init('/script/search.min.js')
   var old_value
   
   loading.style.visibility = 'hidden'
@@ -11,7 +14,7 @@
   search_worker.on('populate', function (post) {
     var div = document.createElement('div')
     div.className = 'result'
-    div.innerHTML = '<h4><a href=/' + post.id + '.html>' + post.title + '</a></h4><p>' + post.snip + '...</p>'
+    div.innerHTML = '<h4><a href=/post/' + post.id + '.html>' + post.title + '</a></h4><p>' + post.snip + '...</p>'
     results.appendChild(div)
   })
   
@@ -37,9 +40,33 @@
       }
     }
   }
+
+  function random_stuff () {
+    if (Math.random() < .05) {
+      var name = $('header').lastChild.lastChild
+      console.log(name)
+      switch (0|Math.random() * 10) {
+        case 1:
+          name.className = 'author weird-spin'
+          name.innerHTML = 'Wlex Ailson'
+        break;
+        case 2:
+          name.className = 'author weird-flip'
+        break;
+        break;
+        case 2:
+          name.className = 'author weird-grow'
+        break;
+      }
+
+    }
+  }
+
   
   setTimeout(function () {
     check_images()
     window.onscroll = check_images
+
+    random_stuff()
   }, 100)
 }())
